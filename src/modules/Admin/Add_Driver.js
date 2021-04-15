f51955db50eb13b625421b413470d722e69748bc
vidya13sagar <vidyasagar@nineleaps.com>
2021-04-15 08:32:34 +0530

    [conflicts2]

--- src/modules/Admin/Add_Driver.js
+++ src/modules/Admin/Add_Driver.js
@@ -287,6 +287,81 @@ export default function Add_Driver({ navigation }) {
                   </TouchableOpacity>
                 </View>
               </View>
+            </Modal>
+            <View style={{ flexDirection: 'row', marginBottom: '35%', marginLeft: 10, marginTop: 10 }}>
+              <TouchableOpacity onPress={backpress} style={{ justifyContent: 'flex-start' }}><Ionicons name="arrow-back"
+                color="#FFF" size={25}
+                style={styles.icon}
+              /></TouchableOpacity>
+              <TouchableOpacity onPress={pick} style={{ marginLeft: '80%' }}><Ionicons name="create"
+                color="#FFF" size={25}
+                style={styles.icon}
+              /></TouchableOpacity>
+            </View>
+            <Image style={{ width: '100%', height: '50%', justifyContent: 'center' }} source={{ uri: img }} />
+          </View>
+          : <View>
+            <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+              <TouchableOpacity onPress={press} >
+                <Image style={styles.licence} source={{ uri: img }} />
+                {/* <Ionicons name="camera"
+      color="white" size={20}
+      style={{backgroundColor:'#FF5C00',marginTop:90,borderRadius:25,justifyContent:'flex-end',alignSelf:'flex-end'}}
+      /> */}
+              </TouchableOpacity>
+            </View>
+
+          </View>
+        }
+        <View>
+          <Text style={styles.TextInput4}>
+            uploade image
+           </Text>
+        </View>
+        </View>
+        <View>
+        {pic1 ?
+          <View style={{width: "100%", height: "100%",   backgroundColor: 'black' }}>
+            <Modal animationType="slide"  transparent={true} visible={modalVisible1}>
+              <View style={styles.modalContainer}>
+                <Ionicons
+                  name="close-circle-outline"
+                  color="#fff"
+                  size={30}
+                  style={styles.icon}
+                  onPress={(modalVisible1) => setModalVisible1(!modalVisible1)}
+                />
+                <View style={styles.modalBody1}>
+                  <TouchableOpacity
+                    style={{ alignSelf: 'center', marginTop: 5 }}
+                    onPress={Camera1}>
+                    <Text
+                      style={{
+                        color: '#1E90FF',
+                        fontSize: 19,
+                      }}>
+                      Open Camera <Ionicons name="camera"
+                        color="#1E90FF" size={25}
+                        style={styles.icon}
+                      />
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{ alignSelf: 'center', marginTop: 20 }}
+                    onPress={gallery1}>
+                    <Text
+                      style={{
+                        color: '#1E90FF',
+                        fontSize: 19,
+                      }}>
+                      Choose From Gallery <Ionicons name="folder"
+                        color="#1E90FF" size={25}
+                        style={styles.icon}
+                      />
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
             </Modal>        
           </View>
           : <View>
